871f81bcbb299d4e4aeb824cb2deabec81291e3b
Dominik <dgolab@gmail.com>
2018-03-07 18:15:17 +0100

    Nowy commit Kamil

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -1,5 +1,7 @@
 package pl.edu.ur.main;
 
+import java.util.Scanner;
+
 /**
  *
  * @author marcin
@@ -11,6 +13,63 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO zad 3, 4, 6, 7
+    /*    
+    Scanner miesiac = new Scanner(System.in);
+    int a;
+    a=miesiac.nextInt();
+    while(a!=0){
+    switch (a) {
+        case 1: System.out.println("Styczen");
+        break;
+        case 2: System.out.println("Luty");
+        break;
+        case 3: System.out.println("Marzec");
+        break;
+        case 4: System.out.println("Kwiecien");
+        break;
+        case 5: System.out.println("Maj");
+        break;
+        case 6: System.out.println("Czewriec");
+        break;
+        case 7: System.out.println("Lipiec");
+        break;
+        case 8: System.out.println("Sierpien");
+        break;
+        case 9: System.out.println("Wrzesien");
+        break;
+        case 10: System.out.println("Pazdziernik");
+        break;
+        case 11: System.out.println("Listopad");
+        break;
+        case 12: System.out.println("Grudzien");
+        break;
+}
+} int a=0;
+    int i=1;
+    int b=1;
+    
+      
+    do {
+        a += b;
+        i+=1;
+        System.out.println("petla nr "+(i));
+        System.out.println("a = "+ a);
+    } while(a<1000);
+    
+    int i;
+    for (i=20;i>=0;i--){
+        if(i==2 || i==6 || i=9 || i=15 || i==19){
+            continue;
+        }
+        System.out.println("liczba "+i);
     }
+ */
+    Scanner odczyt = new Scanner(System.in);
+    int a=odczyt.nextInt();
+    int b=odczyt.nextInt();
+    int c=odczyt.nextInt();
+    int d=b*b-4*a*c;
+    System.out.println("Delta = "+d);
+            }
     
 }
